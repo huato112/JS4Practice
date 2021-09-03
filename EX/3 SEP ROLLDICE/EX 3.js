9836f0e54c0fc5836cbf4a483fce81a58ad4a794
Amornpong Duangchawee <amornpong.213@gmail.com>
2021-09-03 16:42:01 +0700

    ROLL DICE

---
+++ EX/3 SEP ROLLDICE/EX 3.js
@@ -0,0 +1,33 @@
+let player1 = {name: "alice", roll: [], result: [] };
+let player2 = {name: "bob", roll: [], result: [] };
+let count = 0;
+let dice = {roll:function(){return Math.floor(Math.random() * 6) + 1}}
+
+// function rollDice(){
+//     return Math.floor(Math.random() * 6) + 1 //return random (0-5) + 1
+// }
+
+function gameResult(roll1,roll2){
+
+    if(roll1 > roll2){
+        player1.result[count] = "WIN";
+        player2.result[count] = "LOSE";
+    }
+    else if(roll1 < roll2){
+        player1.result[count] = "LOSE";
+        player2.result[count] = "WIN";
+    }
+    else{
+        player1.result[count] = "DRAW";
+        player2.result[count] = "DRAW";
+    }
+    player1.roll[count] = roll1;
+    player2.roll[count] = roll2;
+    count++;
+}
+
+gameResult(dice.roll(),dice.roll());
+gameResult(dice.roll(),dice.roll());
+gameResult(dice.roll(),dice.roll());
+console.log(player2);
+console.log(player1);
