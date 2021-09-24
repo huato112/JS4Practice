fb255f1e90a97f2ff8117803e5e83b99dcf0bf2a
Amornpong Duangchawee <amornpong.213@gmail.com>
2021-09-24 17:01:25 +0700

    #06

---
+++ EX/24 SEP/ReduceMapFunc.js
@@ -0,0 +1,16 @@
+//examples of reduce and map functions
+let products = [
+  { prodId: 1, price: 10, amount: 2 },
+  { prodId: 2, price: 5, amount: 10 }
+];
+
+let priceIncludeVat = products.map(
+  (product) => product.price + product.price * 0.07
+);
+console.log(priceIncludeVat);
+
+let totalAmt = products.reduce(
+  (total, curProd) => (total += curProd.amount),
+  0
+);
+console.log(totalAmt);
--- EX/24 SEP/ex.js
+++ EX/24 SEP/ex.js
@@ -8,3 +8,16 @@ const sum = function (numbers) {
     for (let num of numbers) total += num;
     return total;
   };
+
+  //examples of arguments object
+let products = [
+  { prodId: 1, price: 10, amount: 2 },
+  { prodId: 2, price: 5, amount: 10 }
+];
+
+function getProducts() {
+  for (let i = 0; i < arguments.length; i++) {
+    console.log(arguments[i]);
+  }
+}
+getProducts(products);
