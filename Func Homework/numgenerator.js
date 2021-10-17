22f6b27e2e6138575b0b806599b86c3576364930
Amornpong Duangchawee <amornpong.213@gmail.com>
2021-10-18 00:41:38 +0700

    FUNC HOMEWORD UPDATED

--- Func Homework/numgenerator.js
+++ Func Homework/numgenerator.js
@@ -1,9 +1,18 @@
-function numberGenerator(range = 0, range2 = 2){ //Func สุ่มเลขตามที่เรากำหนด
-    return function genarete(multiply = 1){  // option เสริมไว้คูณ
-        let result = -1
-        result = Math.ceil(Math.random() * (range2 - range + 1) + range - 1 ); // สุ่มเลขตาม range ที่เรากำหนด
+function number(fn, a= 0, b=2){
+    return fn(a, b);
+}
+function add(a, b) {
+    return a + b; 
+}
+function sub(a, b) {
+    return a - b;
+}
+
+function numberGenerator(range, range2){ //Func สุ่มเลขตามที่เรากำหนด
+    let result = Math.floor(Math.random() * (range2 - range + 1) + range); // สุ่มเลขตาม range ที่เรากำหนด
+    return function generate(multiply = 1){  // option เสริมไว้คูณ
         return result*multiply; // คุณกับตัวคูณถ้าเรากำหนดมา
     }
 }
 
-export {numberGenerator} //ทำให้อีกไฟล์นึง import ไปใช้ได้
+export {number, numberGenerator, add, sub} //ทำให้อีกไฟล์นึง import ไปใช้ได้
--- Func Homework/test.js
+++ Func Homework/test.js
@@ -1,12 +1,15 @@
-import {numberGenerator} from "./numgenerator.js"; // import func มาใช้
+import {number, numberGenerator, add, sub} from "./numgenerator.js"; // import func มาใช้
 
 
-let a = numberGenerator(); //เรียก func มาเก็บในตัวแปร a ใน a ก็จะมี func multiply รอครับ
-console.log(a()); 
+let a = number(numberGenerator); //เรียก func มาเก็บในตัวแปร a ใน a ก็จะมี func multiply รอครับ
+console.log(`RANNUM: ${a()}`);
 
-let b = numberGenerator(5,10); //เก็บ range 5, 10 ใน b ไว้รอเรียกกก
-console.log(b());
-console.log(b(2));
+let b = number(numberGenerator, 5, 10); //เก็บ range 5, 10 ใน b ไว้รอเรียกกก
+console.log(`RANNUM: ${b()}`);
+console.log(`RANNUM*2 : ${b(2)}`);
 
-let c = numberGenerator(8,10)(2); //อันนี้เรียกแบบเก็บปุ๊บเรียบปั๊บ ครบจบ
-console.log(c);
+let c = number(add, 5, 12);
+console.log(`ADD: ${c}`);
+
+let d = number(sub, 2, 1);
+console.log(`SUB: ${d}`);
