d579070a934cb4bf757f04a0ee618d2a6c557174
Amornpong Duangchawee <amornpong.213@gmail.com>
2021-10-30 20:42:52 +0700

    z

--- Project 7/product.js
+++ Project 7/product.js
@@ -1,138 +1,95 @@
 export let sampleData = [
   
+  // {
+  //   Id : "M1",
+  //   Name: "Sarin",
+  //   Price: 1500.0,
+  //   img: "img/Sarin.jpg",
+  //   Status: "ว่าง"
+  // },
+  // {
+  //   Id : "M2",
+  //   Name: "Darun",
+  //   Price: 1499.5,
+  //   img: "img/Darun.jpg",
+  //   Status: "ว่าง"
+  // },
+  // {
+  //   Id : "M3",
+  //   Name: "Athiti",
+  //   Price: 92400.0,
+  //   img: "img/Athiti.jpg",
+  //   info: "หล่อเป็นพิเศษ",
+  //   Status: "คืนนี้คิวเต็ม"
+  // },
+  // {
+  //   Id : "M4",
+  //   Name: "Udomchai",
+  //   Price: 2000.5,
+  //   img: "img/Udomchai.jpg",
+  //   Status: "ว่าง"
+  // },
   {
-    Id : "M1",
-    Name: "Sarin",
-    Price: 1500.0,
-    img: "img/Sarin.jpg",
-    Status: "ว่าง"
+    Id: "G001",
+    Name: "Grand Theft Auto V",
+    Price: 6000,
+    img: "gimg/gtav.jpg",
+    Status: "in stock",
   },
   {
-    Id : "M2",
-    Name: "Darun",
-    Price: 1499.5,
-    img: "img/Darun.jpg",
-    Status: "ว่าง"
+    Id: "G002",
+    Name: "Cities Skyline",
+    Price: 4600,
+    img: "gimg/ctsl.jpg",
+    Status: "In stock",
   },
   {
-    Id : "M3",
-    Name: "Athiti",
-    Price: 92400.0,
-    img: "img/Athiti.jpg",
-    info: "หล่อเป็นพิเศษ",
-    Status: "คืนนี้คิวเต็ม"
+    Id: "G003",
+    Name: "Fall Guys: Ultimate Knockout",
+    Price: 6000,
+    img: "gimg/fg.jpg",
+    Status: "In stock",
   },
   {
-    Id : "M4",
-    Name: "Udomchai",
-    Price: 2000.5,
-    img: "img/Udomchai.jpg",
-    Status: "ว่าง"
-  },
-  //ซ้ำอีกชุุด
-  {
-    Id : "M5",
-    Name: "Saridfgn",
-    Price: 1500.0,
-    img: "img/Sarin.jpg",
-    Status: "ว่าง"
+    Id: "G004",
+    Name: "Home Sweet Home",
+    Price: 6000,
+    img: "gimg/hsh.jpg",
+    Status: "In stock",
   },
   {
-    Id : "M6",
-    Name: "Darsdfgun",
-    Price: 1499.5,
-    img: "img/Darun.jpg",
-    Status: "ว่าง"
+    Id: "G005",
+    Name: "Minecraft",
+    Price: 9900,
+    img: "gimg/minecraft.jpg",
+    Status: "In stock",
   },
   {
-    Id : "M7",
-    Name: "Athiti",
-    Price: 2400.0,
-    img: "img/Athiti.jpg",
-    Status: "ว่าง"
+    Id: "G007",
+    Name: "OverWatch",
+    Price: 6000,
+    img: "gimg/overwatch.jpg",
+    Status: "Out of stock",
   },
   {
-    Id : "M8",
-    Name: "Udomchai",
-    Price: 2000.5,
-    img: "img/Udomchai.jpg",
-    Status: "ว่าง"
+    Id: "G008",
+    Name: "PlayerUnknown's Battlegrounds",
+    Price: 6000,
+    img: "gimg/pubg.jpg",
+    Status: "In stock",
   },
   {
-    Id : "M7",
-    Name: "Athiti",
-    Price: 2400.0,
-    img: "img/Athiti.jpg",
-    Status: "ว่าง"
+    Id: "G009",
+    Name: "Plants vs Zombies",
+    Price: 4200,
+    img: "gimg/pvz.jpg",
+    Status: "In stock",
   },
   {
-    Id : "M8",
-    Name: "Udomchai",
-    Price: 2000.5,
-    img: "img/Udomchai.jpg",
-    Status: "ว่าง"
-  },
-  {
-    Id : "M7",
-    Name: "Athiti",
-    Price: 2400.0,
-    img: "img/Athiti.jpg",
-    Status: "ว่าง"
-  },
-  {
-    Id : "M8",
-    Name: "Udomchai",
-    Price: 2000.5,
-    img: "img/Udomchai.jpg",
-    Status: "ว่าง"
-  },
-  {
-    Id : "M7",
-    Name: "Athiti",
-    Price: 2400.0,
-    img: "img/Athiti.jpg",
-    Status: "ว่าง"
-  },
-  {
-    Id : "M8",
-    Name: "Udomchai",
-    Price: 2000.5,
-    img: "img/Udomchai.jpg",
-    Status: "ว่าง"
-  },
-  {
-    Id : "M7",
-    Name: "Athiti",
-    Price: 2400.0,
-    img: "img/Athiti.jpg",
-    Status: "ว่าง"
-  },
-  {
-    Id : "M8",
-    Name: "Udomchai",
-    Price: 2000.5,
-    img: "img/Udomchai.jpg",
-    Status: "ว่าง"
-  },
-  {
-    Id : "M7",
-    Name: "Athiti",
-    Price: 2400.0,
-    img: "img/Athiti.jpg",
-    Status: "ว่าง"
-  },
-  {
-    Id : "M8",
-    Name: "Udomchai",
-    Price: 2000.5,
-    img: "img/Udomchai.jpg",
-    Status: "ว่าง"
-  },
-  {
-    Id : "M7",
-    Name: "Athiti",
-    Price: 2400.0,
-    img: "img/Athiti.jpg",
-    Status: "ว่าง"
+    Id: "G010",
+    Name: "The sims 4",
+    Price: 6000,
+    img: "gimg/thesim4.jpg",
+    Status: "In stock",
   }
 ];
--- Project 7/test.js
+++ Project 7/test.js
@@ -5,53 +5,58 @@ let newDiv;
 let num = 0;
 
 sampleData.forEach((value) => {
-  if (num == 5) {
-    num = 0;
-  }
+  // if (num == 5) {
+  //   num = 0;
+  // }
 
   if (num == 0) {
     newDiv = document.createElement("div"); //create <div></div>
-    newDiv.setAttribute("class", "d-flex justify-content-center mw-100");
+    newDiv.setAttribute("class", "row row-cols-2 row-cols-lg-5 g-2 g-lg-3");
+    num = num + 1;
   }
 
-  num = num + 1;
+  // num = num + 1;
   //create img
   let img = document.createElement("img");
   img.src = `${value.img}`;
   //img.className = "img-fluid"
-  img.width = "150"
-  img.height = "150"
+  //img.width = "150"
+  img.height = "250"
 
 
   const contentDiv = document.createElement("div");
-  contentDiv.setAttribute("class", "shadow p-3 mb-5 bg-body rounded p-2 bd-highlight card mh-100 px-md-4 py-md-4 mx-md-4 my-md-4");
+  contentDiv.setAttribute("class", "col shadow bg-body rounded p-2 bd-highlight mh-100 px-md-4 py-md-4 mx-md-4 my-md-4");
   contentDiv.appendChild(img); //ใส่รูปอิอิ
-  contentDiv.appendChild(document.createElement("br"));
-  contentDiv.appendChild(document.createTextNode(`ID: ${value.Id}`)); //ใส่ข้อมูลใน div ย่อย
-  contentDiv.appendChild(document.createElement("br"));
-  contentDiv.appendChild(document.createTextNode(`Name: ${value.Name}`)); //ใส่ข้อมูลใน div ย่อย
-  contentDiv.appendChild(document.createElement("br"));
-  contentDiv.appendChild(document.createTextNode(`Price: ${value.Price}`));
 
-  if (typeof value.info !== "undefined"){
-    contentDiv.appendChild(document.createElement("br"));
-    contentDiv.appendChild(document.createTextNode(`Info: ${value.info}`))
-  }else{
-    contentDiv.appendChild(document.createElement("br"));
-    contentDiv.appendChild(document.createTextNode(`Info: -`))
-  }
-  contentDiv.appendChild(document.createElement("br"));
-  contentDiv.appendChild(document.createTextNode(`Status: ${value.Status}`))
+  const content2div = document.createElement("div");
+ 
+  content2div.appendChild(document.createElement("br"));
+  content2div.appendChild(document.createTextNode(`ID: ${value.Id}`)); //ใส่ข้อมูลใน div ย่อย
+  content2div.appendChild(document.createElement("br"));
+  content2div.appendChild(document.createTextNode(`Name: ${value.Name}`)); //ใส่ข้อมูลใน div ย่อย
+  content2div.appendChild(document.createElement("br"));
+  content2div.appendChild(document.createTextNode(`Price: ${value.Price}`));
+  content2div.appendChild(document.createElement("br"));
+  content2div.appendChild(document.createTextNode(`Status: ${value.Status}`))
+  content2div.appendChild(document.createElement("br"));
   // class btn btn-primary
   // create button
-
+  // class btn btn-danger
+  // cursor: not-allowed
   let btn = document.createElement("a");
+  if (value.Status == "Out of stock"){
+    
+    btn.setAttribute("class", "btn btn-danger mw-100 cursor-not-allowed");
+    btn.setAttribute("style", "cursor: not-allowed");
+    btn.appendChild(document.createTextNode("Out of stock"))
+  } else{
   btn.setAttribute("class", "btn btn-primary mw-100");
   btn.setAttribute("href", "https://www.google.com");
   btn.setAttribute("target", "_blank");
-  btn.appendChild(document.createTextNode("1 Night"))
-  contentDiv.appendChild(btn);
-
+  btn.appendChild(document.createTextNode("Add to cart"))
+}
+  content2div.appendChild(btn);
+  contentDiv.appendChild(content2div);
   newDiv.appendChild(contentDiv);
 
   document.getElementById("Product").appendChild(newDiv); //เอา div ใส่ product
