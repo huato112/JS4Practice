ec41270aaa6cb260d271d20efd2786001e45de83
Amornpong Duangchawee <amornpong.213@gmail.com>
2021-11-24 01:29:36 +0700

    siripong

--- Project 7/theme.js
+++ Project 7/theme.js
@@ -1,8 +1,10 @@
+import * as cookieutil from "./cookieutil.js";
+
 let theme = document.getElementsByClassName("theme-set")[0];
 theme.addEventListener("click", select);
 
 let count = `light`;;
-loadTheme(getCookie(`theme`)); //dark
+loadTheme(cookieutil.getCookie(`theme`)); //dark
 
 function loadTheme(fn){
   if(fn == `dark`){
@@ -13,11 +15,11 @@ function loadTheme(fn){
   }
   function light(){
     document.body.className = "bg-white text-dark";
-    setCookie('theme', `light`, 1000)
+    cookieutil.setCookie('theme', `light`, 1000)
 }
   function dark(){
       document.body.className = "bg-secondary";
-      setCookie('theme', `dark`, 1000)
+      cookieutil.setCookie('theme', `dark`, 1000)
   }
   
 }
@@ -31,30 +33,4 @@ function select(){
         count = `dark`;
         return loadTheme(`dark`);
     }
-}
-
-function getCookie(cname) {
-    let name = cname + "=";
-    let ca = document.cookie.split(';');
-    for(let i = 0; i < ca.length; i++) {
-      let c = ca[i];
-      while (c.charAt(0) == ' ') {
-        c = c.substring(1);
-      }
-      if (c.indexOf(name) == 0) {
-        return c.substring(name.length, c.length);
-      }
-    }
-    return "";
-}
-
-function setCookie(cname, cvalue, exdays) {
-    const d = new Date();
-    d.setTime(d.getTime() + (exdays * 24 * 60 * 60 * 1000));
-    let expires = "expires="+d.toUTCString();
-    document.cookie = cname + "=" + cvalue + ";" + expires + ";path=/";
-  }
-
-function unsetCookie(cname) {
-  document.cookie = `${cname}=; expires=Thu, 01 Jan 1970 00:00:00 UTC; path=/;`;
 }
